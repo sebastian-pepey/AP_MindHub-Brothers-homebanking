329a0c9ed655c7868ded088cdb912a4f52f8aa32
sebastian-pepey <robertopepey@gmail.com>
2023-08-31 23:32:31 -0300

    Correction of Controllers and Service Layer

--- src/main/java/com/mindhub/homebanking/services/AccountService.java
+++ src/main/java/com/mindhub/homebanking/services/AccountService.java
@@ -1,15 +1,8 @@
 package com.mindhub.homebanking.services;
-
 import com.mindhub.homebanking.dtos.AccountDTO;
-import com.mindhub.homebanking.repositories.AccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public interface AccountService {
     List<AccountDTO> getAccounts();
--- src/main/java/com/mindhub/homebanking/services/implement/AccountServiceImplement.java
+++ src/main/java/com/mindhub/homebanking/services/implement/AccountServiceImplement.java
@@ -4,8 +4,6 @@ import com.mindhub.homebanking.dtos.AccountDTO;
 import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
